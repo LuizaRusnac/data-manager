9b0a34550e9039f34a5fc4e49cc6ef30f312f7a1
LuizaRusnac <luiza.anaa@yahoo.com>
2024-04-13 13:01:20 +0300

    Modified setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pd-data-manager',
-    version='0.1.1',
+    name='dforge',
+    version='1.0.0',
     packages=find_packages(),
     author='Luiza Rusnac',
     author_email='luiza.rusnac93@gmail.com',
     description='A package for efortless data manager.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/LuizaRusnac/data-manager.git',
+    url='https://github.com/LuizaRusnac/dforge.git',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Programming Language :: Python :: 3',
